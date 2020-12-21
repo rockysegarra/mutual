8aa02d0b5f728d1a62fedbeedc05eaf18c3ac694
Chris <csantos0822@gmail.com>
2020-12-21 16:28:20 -0500

    Filtering

--- src/app/main/sample/sample.component.ts
+++ src/app/main/sample/sample.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, PipeTransform } from "@angular/core";
+import { FormControl } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
 import { Member } from './models/member';
 import { MembersService } from './services/members.service';
 
@@ -11,6 +13,10 @@ export class SampleComponent {
     title = "angular-datatables";
 
     rows:Member[];
+    filter = new FormControl('');
+    public rows$ = new BehaviorSubject(null);
+
+
 
     constructor(private membersService: MembersService){
 
@@ -18,13 +24,29 @@ export class SampleComponent {
 
     ngOnInit() {
         this.fetchMembers();
+        this.filter.valueChanges.subscribe(
+            result => 
+            {
+                this.rows$.next(this.search(result));
+            }
+      );
 
     }
 
     fetchMembers() {
         this.membersService.getMembers().subscribe(res =>{
-            this.rows = res.data
+            this.rows = res.data;
+            console.log(this.rows);
+            this.rows$.next(this.rows);
 
         });
     }
+    search(text: string, pipe?: PipeTransform): Member[] {
+        return this.rows.filter(c => {
+          const term = text.toLowerCase();
+          return c.memberName.toLowerCase().includes(term)
+              || c.memberNumber.toLowerCase().includes(term)
+              || c.createdDt.toLowerCase().includes(term)
+        });
+      }
 }
